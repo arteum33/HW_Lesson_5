9a6050c3df29f4c7222ad1d3a5c0cfe5c33fa133
arteum33 <arteum33@gmail.com>
2021-10-17 14:39:35 +0300

    Branch with functions tests for Homework #6 Part with hand test

---
+++ Hand_test_fuctions.py
@@ -0,0 +1,23 @@
+# Homework №6
+# hand test of functions from homework 5
+
+import divisor_master
+
+
+
+# 1. написать программу
+# 2. отдельно проверить функцию
+#print(hello_who('Max'))
+
+# salary(2, 10) == 20
+# print(salary(2, 10))
+
+# С помощью if
+# if salary(2, 10) != 20:
+#     print('Failed')
+# if hello_who('Max') != 'Hello, Max':
+#     print('Failed')
+# if hello_who('Leo') != 'Hello, Leo':
+#     print('Failed')
+#
+# print('Ok')
